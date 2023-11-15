2149a291a26ab9462106ba1400634db3154afcb7
AnHieu002 <hieuavph20284@fpt.edu.vn>
2023-11-15 23:39:02 +0700

    kieudet

--- src/main/java/com/example/datn/Request/TimKiemRequest.java
+++ src/main/java/com/example/datn/Request/TimKiemRequest.java
@@ -15,4 +15,5 @@ import java.util.List;
 public class TimKiemRequest {
     @JsonProperty("mauSacId")
     private List<Integer> mauSacId;
+    private List<Integer> kieuDetId;
 }
--- src/main/java/com/example/datn/Response/TimKiemSettingResponse.java
+++ src/main/java/com/example/datn/Response/TimKiemSettingResponse.java
@@ -1,5 +1,6 @@
 package com.example.datn.Response;
 
+import com.example.datn.Entity.KieuDet;
 import com.example.datn.Entity.MauSac;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,4 +15,5 @@ import java.util.List;
 @Builder
 public class TimKiemSettingResponse {
     private List<MauSac>listmauSac;
+    private List<KieuDet>listkieuDet;
 }
