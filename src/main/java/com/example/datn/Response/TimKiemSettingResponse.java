a8f8d3e98a352a6f1cb2dddc563252862ea1eb48
congnt29N1 <143944102+congnt29N1@users.noreply.github.com>
2023-11-19 23:46:32 +0700

    Revert "update ctsp"
    This reverts commit 19c00bf8b9b8d9ac7b320bd232098db3bcc5bb65.

--- src/main/java/com/example/datn/Response/TimKiemSettingResponse.java
+++ src/main/java/com/example/datn/Response/TimKiemSettingResponse.java
@@ -4,15 +4,12 @@ package com.example.datn.Response;
 import com.example.datn.Entity.CauTrucKhuy;
 import com.example.datn.Entity.ChatLieu;
 import com.example.datn.Entity.DanhMuc;
-import com.example.datn.Entity.HoaTiet;
 import com.example.datn.Entity.KhuyenMai;
 import com.example.datn.Entity.KichCo;
 import com.example.datn.Entity.KieuDet;
-import com.example.datn.Entity.KieuTui;
 import com.example.datn.Entity.LopLot;
 import com.example.datn.Entity.MauSac;
 import com.example.datn.Entity.VeAo;
-import com.example.datn.Entity.XeTa;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,11 +29,8 @@ public class TimKiemSettingResponse {
     private List<KhuyenMai> listKhuyenMai;
     private List<MauSac> listMauSac;
     private List<KieuDet> listkieuDet;
-    private List<KieuTui> listKieuTui;
     private List<ChatLieu> listchatLieu;
-    private List<HoaTiet> listHoaTiet;
     private List<VeAo> listveAo;
-    private List<XeTa> listXeTa;
     private List<LopLot> listlopLot;
     private List<CauTrucKhuy> listcauTrucKhuy;
 
