9b320e2d0e950ac2f274438cc47236c4faaf4476
ĐỖ PHƯƠNG NAM <namdp1207@gmail.com>
2023-11-20 14:20:42 +0700

    sigup v2

--- src/main/java/com/example/datn/Security/SecurityConfiguration.java
+++ src/main/java/com/example/datn/Security/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package com.example.datn.Security;
 
-import com.example.datn.repository.NhanVienRepository;
+import com.example.datn.Repository.NhanVienRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
